63bdb1907f8ad5d05b6526d2f5465bb10064104c
Sergio Moreno <sergiomoreno433@gmail.com>
2020-10-15 17:50:24 -0500

    Shows the sum of n numbers stored in a slice

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+	var n uint64
+	fmt.Scanln(&n)
+	s := make([]int64,n)
+	result := int64(0)
+	for i := 0; i < int(n); i++ {
+		fmt.Scanln(&s[i])
+		result += s[i]
+	}
+	fmt.Println(result)
+
+
 }
